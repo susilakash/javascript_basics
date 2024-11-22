be6f96c605c9183c4e87293d3d41638577af7060
Susil <susilakashraj100@gmail.com>
2024-11-22 23:37:28 +0530

    prefix operator

---
+++ basic_concepts/prefix_operator.js
@@ -0,0 +1,9 @@
+//prefix operator increments the value by 1 and then returns the updated value
+//prefix increment
+let a = 10;
+let b = ++a; //b=11 a=11
+console.log("a + b = ", a + b, "a = ", a, "b = ", b)
+//prefix decrement
+let x = 20;
+let y = --x; //x=19 y=19
+console.log("x - y = ", x - y, "x = ", x, "y = ", y)
